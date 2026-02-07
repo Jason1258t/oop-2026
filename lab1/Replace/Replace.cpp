c16319d7dc850361fd78bf4b95423a0aa731c669
Jason <ivandomogatskij@gmail.com>
2026-02-07 22:37:13 +0300

    init

---
+++ lab1/Replace/Replace.cpp
@@ -0,0 +1,70 @@
+#include <fstream>
+#include <iostream>
+#include <stdio.h>
+#include <string>
+
+// Возвращает результат замены всех вхождения строки searchString внутри строки subject на replacementString
+// Если строка searchString пустая, то возвращается subject
+std::string ReplaceString(const std::string& subject,
+	const std::string& searchString, const std::string& replacementString)
+{
+
+	size_t pos = 0;
+
+	std::string result;
+	while (pos < subject.length())
+	{
+		size_t foundPos = subject.find(searchString, pos);
+		if (foundPos != std::string::npos)
+		{
+			result.append(subject, pos, foundPos - pos);
+			result.append(replacementString);
+			pos = foundPos + searchString.length();
+		}
+		else
+		{
+			result.append(subject, pos, subject.length() - pos);
+			break;
+		}
+	}
+	return result;
+}
+
+void CopyStreamWithReplacement(std::istream& input, std::ostream& output,
+	const std::string& searchString, const std::string& replacementString)
+{
+	std::string line;
+
+	while (std::getline(input, line))
+	{
+		output << ReplaceString(line, searchString, replacementString) << "\n";
+	}
+}
+
+int main(int argc, char* argv[])
+{
+	if (argc != 5)
+	{
+		std::cout << "Invalid argument count\n"
+				  << "Usage: replace.exe <inputFile> <outputFile> <searchString> <replacementString>\n";
+		return 1;
+	}
+
+	// Самостоятельно выделите код копирования содержимого файла в отдельную функцию CopyFileWithReplacement,
+	// принимающую имена файлов, а также строки для поиска и замены
+	// Добавьте обработку ошибок
+
+	std::ifstream inputFile;
+	inputFile.open(argv[1]);
+
+	std::ofstream outputFile;
+	outputFile.open(argv[2]);
+
+	std::string search = argv[3];
+	std::string replace = argv[4];
+
+	CopyStreamWithReplacement(inputFile, outputFile, search, replace);
+	outputFile.flush();
+
+	return 0;
+}
