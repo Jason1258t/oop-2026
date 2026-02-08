4b060fda148fbd519736d25bf169d4136fb7885d
Jason <ivandomogatskij@gmail.com>
2026-02-08 22:56:48 +0300

    выглядит здраво крч, надо сделать нормальную обработку ошибок в соответствии с заданием и добавить работу со вводом stdin

--- lab1/Replace/Replace.cpp
+++ lab1/Replace/Replace.cpp
@@ -3,39 +3,16 @@
 #include <stdio.h>
 #include <string>
 
-// Возвращает результат замены всех вхождения строки searchString внутри строки subject на replacementString
-// Если строка searchString пустая, то возвращается subject
-std::string ReplaceString(const std::string& subject,
-	const std::string& searchString, const std::string& replacementString)
-{
-	if (searchString.empty())
-	{
-		return subject;
-	}
-	size_t pos = 0;
-
-	std::string result;
-	while (pos < subject.length())
-	{
-		size_t foundPos = subject.find(searchString, pos);
-		if (foundPos != std::string::npos)
-		{
-			result.append(subject, pos, foundPos - pos);
-			result.append(replacementString);
-			pos = foundPos + searchString.length();
-		}
-		else
-		{
-			result.append(subject, pos, subject.length() - pos);
-			break;
-		}
-	}
-	return result;
-}
 
 void CopyStreamWithReplacement(std::istream& input, std::ostream& output,
     const std::string& searchString, const std::string& replacementString)
 {
+	if (searchString.empty())
+	{
+		output << input.rdbuf();
+		return;
+	}
+
     std::string buffer;
     char c;
 
@@ -79,23 +56,19 @@ int main(int argc, char* argv[])
 {
 	if (argc == 2 && std::string(argv[1]) == "-h")
 	{
-		std::cout << "Usage: Replace <inputFile> <outputFile> <searchString> <replacementString>\n";
-		std::cout << "Replaces all occurrences of <searchString> with <replacementString> in <inputFile>.\n";
+		std::cout << "Usage: replace <inputFile> <outputFile> <searchString> <replacementString>" << std::endl;
+		std::cout << "Replaces all occurrences of <searchString> with <replacementString> in <inputFile>." << std::endl;
 		return 0;
 	}
 
 	if (argc != 5)
 	{
-		std::cout << "Invalid argument count\n"
-				  << "Usage: replace.exe <inputFile> <outputFile> <searchString> <replacementString>\n";
+		std::cout << "Invalid argument count" << std::endl
+				  << "Usage: replace <inputFile> <outputFile> <searchString> <replacementString>" << std::endl;
 
-		std::cout << "Passed arguments:\n"
-				  << argc << std::endl;
 		return 1;
 	}
 
-	// Самостоятельно выделите код копирования содержимого файла в отдельную функцию CopyFileWithReplacement,
-	// принимающую имена файлов, а также строки для поиска и замены
 	// Добавьте обработку ошибок
 
 	CopyFileWithReplacement(argv[1], argv[2], argv[3], argv[4]);
